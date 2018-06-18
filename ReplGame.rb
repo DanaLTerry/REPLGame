80eeb587aa7b195b826382a8309c94aa4de7a723
Dana Terry <Dana@Danas-MacBook-Air.local>
2018-06-18 19:54:28 -0400

    Adds a fix to case statements

--- ReplGame.rb
+++ ReplGame.rb
@@ -15,41 +15,43 @@ enter = gets
 system("clear")
 puts "The interviewer asks you to share a little bit about yourself"
 puts "Options: play_it_safe, play The_Game_of_Office_Thrones"
-Play_it_safe = Play_s
-The_Game_of_Office_Thrones = Play_GOOT
+puts "note: play_it_safe is play s and play The_Game_of_Thrones is goot"
+
 option = gets.chomp
 case option
-when play_s
+when "play s"
   puts "you give your pitch which illustrates how your experience is the right fit for this office."
   puts "The interviewer jots notes."
 
-when play_GOOT
+when "goot"
   puts "You say \"Is that meant to insult me?! I am Daenerys Stormborn of House Targaryen, the Unburnt, Mother of Dragons, khaleesi to Drogo's riders, and queen of the Seven Kingdoms of Westeros!\""
   puts "the interviewer calls security."
 end
+
 puts "the interviewer asks \"What is your five-year plan?\""
 puts "Options: Play_it_safe, Play_the_Game_of_Office_Thrones"
-Play_it_safe = Play_s
-Play_the_Game_of_Office_Thrones = Play_GOOT
+
 option = gets.chomp
 case option
-when "play_s"
+when "play s"
     puts "you talk about how your career goals will be met within this position"
     puts "The interviewer jots notes."
-when "play_GOOT"
+    
+when "goot"
     puts "you tip the interviewer out of his/her chair while shouting \"Seize the throne!\" "
     puts "The interviewer yells for security."
 end
+
 puts "the interviewer asks \"What are your strengths?\""
 puts "Options: Play_it_safe, Play_the_Game_of_Office_Thrones"
-Play_it_safe = Play_s
-Play_the_Game_of_Office_Thrones = Play_GOOT
+
 option = gets.chomp
 case option
-when "play_s"
+when "play s"
     puts "you share the strengths which are most applicable to the position and give examples of how you have used them in the past."
     puts "The interviewer jots notes"
-when "play_GOOT"
+    
+when "goot"
     puts "you say \"I drink and I know things.\" "
     puts "The interviewer wonders if you are currently drunk."
 end
