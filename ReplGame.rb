6b09bf23012853656d0e6c1e9c0007cebb8124b5
Dana Terry <Dana@Danas-MacBook-Air.local>
2018-06-21 14:05:05 -0400

    Added more comments, game flow, clearing options to reduce screen clutter

--- ReplGame.rb
+++ ReplGame.rb
@@ -1,11 +1,15 @@
 #RPG Ruby: A comedic way to demonstrate the does and don'ts of handeling an interview via GOT jokes
 #By Dana L Terry
-#intro message/ customizable username
+
+
+#game over variable
 def game_over
   return;
 end
 
 system("clear")
+
+#intro message/ customizable username
 puts "Welcome to Game of Office Thrones!"
 puts "When you play the Game of Office Thrones, you either win or you die."
 puts "....jobless and alone"
@@ -24,6 +28,7 @@ print "Press 'Enter' to proceed"
 enter = gets
 system("clear")
 
+#clears screen variable
 def clear_term
   enter = gets
   system("clear")
@@ -31,6 +36,8 @@ end
 
 #Question 1
 puts "The interviewer asks you to share a little bit about yourself"
+
+#Options
 puts "Options: play_it_safe, play The_Game_of_Office_Thrones"
 puts "note: play_it_safe is play s and play The_Game_of_Thrones is goot"
 
@@ -39,59 +46,116 @@ case option
 when "play s"
   puts "You give your pitch which illustrates how your experience is the right fit for this office."
   
-  part1 = "The interviewer jots notes."
+  part1 = "response1s"
+  print "Press 'Enter' to proceed"
+  enter = gets
 
 when "goot"
   puts "You say \"Is that meant to insult me?! I am Daenerys Stormborn of House Targaryen, the Unburnt, Mother of Dragons, khaleesi to Drogo's riders, 
   and queen of the Seven Kingdoms of Westeros!\""
+  print "Press 'Enter' to proceed"
+  enter = gets
   
-  part1 = "The interviewer calls security."
+  part1 = "response1g"
 end
 
+#clears screen
+clear_term()
 
 Question2 = "The interviewer asks \"What is your five-year plan?\""
-
+Options2 = "Options: Play_it_safe, Play_the_Game_of_Office_Thrones"
 case part1
-when "The interviewer jots notes."
+when "response1s"
   puts "The interviewer jots notes."
   puts Question2
-when "The interviewer calls security."
+  puts Options2
+when "response1g"
+  puts "The interviewer calls security."
+  puts"This isn't Game of Thrones, you crazy person! This is reality!"
+  puts"*Winter is certainly Coming* but you will be left cold, you weirdo! bwahahaha"
   puts "game over"
   abort
 end
-  
-clear_term()
-  
-#How can I clear systems without it jumping over the responses? I don't like how cluttered it looks. 
 
 #Question 2
 
 # Question2 = "The interviewer asks \"What is your five-year plan?\""
-puts "Options: Play_it_safe, Play_the_Game_of_Office_Thrones"
+# puts "Options: Play_it_safe, Play_the_Game_of_Office_Thrones"
 
 option = gets.chomp.strip
 case option 
 when "play s"
     puts "You talk about how your career goals will be met within this position"
-    puts "The interviewer jots notes."
+    
+    part2 = "response2s"
+    print "Press 'Enter' to proceed"
+    enter = gets
     
 when "goot"
     puts "you tip the interviewer out of his/her chair while shouting \"Seize the throne!\" "
-    puts "The interviewer yells for security."
+    
+    part2 = "response2g"
+    print "Press 'Enter' to proceed"
+    enter = gets
+end
+
+#clears screen
+clear_term()
+
+Question3 = "The interviewer asks \"What are your strengths?\""
+Options3 = "Options: Play_it_safe, Play_the_Game_of_Office_Thrones"
+case part2
+when "response2s"
+  puts "The interviewer jots notes again."
+  puts Question3
+  puts Options3
+when "response2g"
+  puts"This isn't Game of Thrones, you crazy person! This is reality!"
+  puts"*Winter is certainly Coming* but you will be left cold, you weirdo! bwahahaha"
+  puts "game over"
+  abort
 end
 
 #Question 3
-Question3 = "The interviewer asks \"What are your strengths?\""
-puts "Options: Play_it_safe, Play_the_Game_of_Office_Thrones"
+# Question3 = "The interviewer asks \"What are your strengths?\""
 
 option = gets.chomp.strip
 case option
 when "play s"
     puts "You share the strengths which are most applicable to the position and give examples of how you have used them in the past."
-    puts "The interviewer jots notes"
+    
+    part3 = "response3s"
+    print "Press 'Enter' to proceed"
+    enter = gets
     
 when "goot"
     puts "You say \"I drink and I know things.\" "
-    puts "The interviewer wonders if you are currently drunk."
+    
+    part3 = "response3g"
+    print "Press 'Enter' to proceed"
+    enter = gets
 end
+
+#clears screen
+clear_term()
+
+#Finals game scripts
+FinalScript_goot = "This isn't Game of Thrones, you crazy person! This is reality!"
+case part3
+when "response3s"
+  puts "The interviewer jots notes once more."
+  puts "Congratulations! You have been added to the pile of acceptable candidates for this position!"
+  puts "If you are lucky, maybe Westeros Corp. will have the human decency to actually send you a rejection email!"
+  puts "'*Winter is still coming* so you have no choice but to try again.... unless you want to be cold or move back in with your parents, sucker! *bwahahahaha!*'" 
+  puts "- so quotes the game of life." 
+  puts "game over"
+  abort
+when "response3g"
+  puts "The interviewer wonders if you are currently drunk."
+  puts FinalScript_goot
+  puts"*Winter is certainly Coming* but you will be left cold, you weirdo! *bwahahahaha!*"
+  puts "game over"
+  abort
+end
+
   
